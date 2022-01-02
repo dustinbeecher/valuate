22d3de178ee449a0a9a1ab739e9133d30a4a8781
dustinbeecher <dustinbeecher@gmail.com>
2022-01-02 05:22:53 -0500

    added auto-updating status

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import Discord, {Intents} from 'discord.js';
-import config from 'config.json';
+const Discord = require('discord.js');
+var config = require('./config.json');
 
 const {Client, Intents} = require('discord.js');
 
@@ -10,8 +10,34 @@ const client = new Discord.Client({
     ]
 });
 
-client.once('ready', () => {
-    console.log("Bot is online");
+client.on('ready', () => {
+    console.log('Bot is online');
+    client.user.setActivity('online!'); 
+    let statuses = [
+        ['and evaluating', 'WATCHING'],
+        ['and evaluating.', 'WATCHING'],
+        ['and evaluating..', 'WATCHING'],
+        ['and evaluating...', 'WATCHING']
+    ];
+    let x = 0;
+    
+    setInterval(() => {
+        if (x === statuses.length) x = 0; {
+            console.log(`${statuses[x][0]} ${statuses[x][1]}`);
+            client.user.setActivity(statuses[x][0], {type: statuses[x][1]}); 
+            x++;
+        }
+    }, 12000); //milliseconds, the rate limit is 5/60s
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ping') {
+        message.reply({
+            content: 'pong',
+        })
+    }
 })
 
 client.login(config.token); //keep this at the bottom of the code
+//YOUR_TOKEN
+//YOUR_PREFIX
