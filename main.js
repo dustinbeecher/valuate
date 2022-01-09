9fa6cea441a44c89635ed6621a83729fbc164af0
dustinbeecher <dustinbeecher@gmail.com>
2022-01-09 09:38:31 -0500

    made code better

--- main.js
+++ main.js
@@ -1,55 +1,51 @@
-//setup
+// setup
 const Discord = require('discord.js');
-var config = require('./config.json');
+const config = require('./config.json');
 
-const {Intents} = require('discord.js');
+const { Intents } = require('discord.js');
 
 const client = new Discord.Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
-    ]
+	intents: [
+		Intents.FLAGS.GUILDS,
+		Intents.FLAGS.GUILD_MESSAGES,
+	],
 });
 
-const botID = 908997485782007859;
+const botID = 908997485782007859n;
 
 let messagesSeen = 0;
 
-//main code
+// main code
 client.on('ready', () => {
-    console.log('Bot is online');
-    client.user.setActivity('online!'); 
-    let x = 0;
-    
-    setInterval(() => {
-        //console.log(messagesSeen);
-        let statuses = [
-            ['WATCHING', 'and evaluating'],
-            ['WATCHING', 'and evaluating.'],
-            ['WATCHING', 'and evaluating..'],
-            ['WATCHING', 'and evaluating...'],
-            ['WATCHING', `${messagesSeen} messages`],
-            ['WATCHING', `${client.guilds.cache.size} servers`],
-            [null, 'FREE JULIAN ASSANGE']
-        ];
-        if (x === statuses.length) x = 0; {
-            //console.log(`${statuses[x][0]} ${statuses[x][1]}`);
-            client.user.setActivity(statuses[x][1], {type: statuses[x][0]}); 
-            x++;
-        }
-    }, 12000); //milliseconds, the rate limit is 5/60s
-})
+	console.log('Bot is online');
+	client.user.setActivity('online!');
+	let x = 0;
+	setInterval(() => {
+		// console.log(messagesSeen);
+		let statuses = [
+			['WATCHING', 'and evaluating'],
+			['WATCHING', 'and evaluating.'],
+			['WATCHING', 'and evaluating..'],
+			['WATCHING', 'and evaluating...'],
+			['WATCHING', `${messagesSeen} messages`],
+			['WATCHING', `${client.guilds.cache.size} servers`],
+		];
+		if (x === statuses.length) x = 0; {
+			// console.log(`${statuses[x][0]} ${statuses[x][1]}`);
+			client.user.setActivity(statuses[x][1], { type: statuses[x][0] });
+			x++;
+		}
+	}, 12000); // milliseconds, the rate limit is 5/60s
+});
 
 client.on('messageCreate', (message) => {
-    if (message.author != botID) {
-        messagesSeen++;
-    }
-    //console.log(messagesSeen);
-    if (message.content === 'ping') {
-        message.reply({
-            content: 'pong',
-        })
-    }
-})
+	if (message.author != botID) {messagesSeen++;}
+	// console.log(messagesSeen);
+	if (message.content === 'ping') {
+		message.reply({
+			content: 'Pong!',
+		});
+	}
+});
 
-client.login(config.token); //keep this at the bottom of the code
+client.login(config.token); // keep this at the bottom of the code
