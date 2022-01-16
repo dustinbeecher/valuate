80283ae25e21b90fecb57bb550b643f6ed0eb4a9
dustinbeecher <dustinbeecher@gmail.com>
2022-01-16 08:11:00 -0500

    optimized server/user info command and added user's tag

--- commands/info.js
+++ commands/info.js
@@ -15,14 +15,28 @@ module.exports = {
 				.setDescription('Server info')),
 	async execute(interaction) {
 		if (interaction.options.getSubcommand() === 'user') {
-			const user = interaction.options.getUser('target');
-			if (user) {
-				await interaction.reply(`${user.username}\n${user.id}`);
-			} else {
-				await interaction.reply(`${interaction.user.username}\n${interaction.user.id}`);
-			}
+			const user = interaction.options.getUser('target') || interaction.user;
+				await interaction.reply(`ID ${user.id}\nUSERNAME ${user.username}\nTAG ${user.discriminator}`);
 		} else if (interaction.options.getSubcommand() === 'server') {
 			await interaction.reply(`${interaction.guild.name}\n${interaction.guild.memberCount} Members\n`);
 		};
 	},
 };
+
+/*
+id
+username
+discriminator
+avatar
+bot
+system
+mfa_enabled
+banner
+accent_color
+locale
+verified
+email
+flags
+premium_type
+public_flags
+*/
